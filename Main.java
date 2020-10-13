3a145c0ad23bd477e2ff5a7ba381d2d9a1442f22
arora-ansh <ansh19022@iiitd.ac.in>
2020-10-13 17:13:32 +0530

    a

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
-/*
-CSE201 - Advanced Programming
-Assignment 3
-
-Name: Ansh Arora
-Roll No: 2019022
-Branch: CSE
-
- */
-
 import java.util.InputMismatchException;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws InputMismatchException {
-        new Game();
+        int x = 1;
+        Scanner in = new Scanner(System.in);
+        do{
+            new Game();
+            System.out.println("\nDo you want to play again? (1-Y 2-N) ");
+            x = in.nextInt();
+        } while(x==1);
     }
 }
